17c02b0501763f43d2f54fb3149878d24b59cc40
AmirBenAhmed <benamir184@gmail.com>
2022-04-07 06:13:07 +0100

    first commit front gestion de stock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {PageLoginComponent} from "./pages/page-login/page-login.component";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { PageInscriptionComponent } from './pages/page-inscription/page-inscription.component';
+import { PageDashboardComponent } from './pages/page-dashboard/page-dashboard.component';
+import { PageStatistiquesComponent } from './pages/page-statistiques/page-statistiques.component';
+import {MenuComponent} from "./composants/menu/menu.component";
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxBootstrapIconsModule,allIcons } from 'ngx-bootstrap-icons';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageLoginComponent,
+    PageInscriptionComponent,
+    PageDashboardComponent,
+    PageStatistiquesComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FontAwesomeModule,
+    AccordionModule.forRoot(),
+    BrowserAnimationsModule,
+    NgxBootstrapIconsModule.pick(allIcons),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/composants/menu/menu.component.ts
@@ -0,0 +1,126 @@
+import {Component, OnInit} from '@angular/core';
+import {Menu} from "./Menu";
+import {Router} from "@angular/router";
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+  expandedIndex = 0;
+  public MenuPropreties: Array<Menu> = [
+    {
+      id: '1',
+      title: 'Tableau de bord',
+      icon: 'fas fa-chart-line',
+      url: '',
+      sousMenu: [
+        {
+          id: '11',
+          title: 'Vue d\'ensemble',
+          icon: 'fas fa-chart-pie',
+          url: '',
+
+        },
+        {
+          id: '12',
+          title: 'Statistiques',
+          icon: 'fas fa-chart-bar',
+          url: '',
+
+        }
+      ]
+    },
+    {
+      id: '2',
+      title: 'Articles',
+      icon: 'fas fa-boxes',
+      url: '',
+      sousMenu:[
+        {
+          id: '21',
+          title: 'Article',
+          icon: 'fas fa-box',
+          url: '',
+        },
+        {
+          id: '22',
+          title: 'Mouvements de stock',
+          icon: 'fa-brands fa-stack-overflow',
+          url: '',
+        }
+      ]
+    },
+    {
+      id: '3',
+      title: 'Clients',
+      icon: 'fas fa-users',
+      url: '',
+      sousMenu:[
+        {
+          id: '31',
+          title: 'Clients',
+          icon: 'fas fa-users',
+          url: '',
+        },
+        {
+          id: '32',
+          title: 'Commande Clients',
+          icon: 'fas fa-basket-shopping',
+          url: '',
+        }
+      ]
+    },
+    {
+      id: '4',
+      title: 'Fournisseurs',
+      icon: 'fas fa-users',
+      url: '',
+      sousMenu:[
+        {
+          id: '41',
+          title: 'Fournisseurs',
+          icon: 'fas fa-users',
+          url: '',
+        },
+        {
+          id: '42',
+          title: 'Commande Fournisseurs',
+          icon: 'fas fa-truck',
+          url: '',
+        }
+      ]
+    },
+    {
+      id: '5',
+      title: 'Parametrages',
+      icon: 'fas fa-cogs',
+      url: '',
+      sousMenu:[
+        {
+          id: '51',
+          title: 'Categories',
+          icon: 'fas fa-tools',
+          url: '',
+        },
+        {
+          id: '52',
+          title: 'Utilisateurs',
+          icon: 'fas fa-users-cog',
+          url: '',
+        }
+      ]
+    }
+  ]
+
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  navigate(url?:string) {
+    this.router.navigate([url]);
+  }
+}
